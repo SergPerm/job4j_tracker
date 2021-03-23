17e2c9cbe7235a168438a2e156c060bd342cadcc
Serg Mokhin <svmokhin@gmail.com>
2021-03-23 22:46:05 +0500

    9. toString [#483460]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,7 @@ public class StartUI {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         Item item = new Item();
+        item.setName("Boris");
 //        System.out.println(item.getCreated().format(formatter));
         Tracker tracker = new Tracker();
         tracker.add(item);
