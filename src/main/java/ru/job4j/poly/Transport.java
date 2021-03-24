9dc6555a6a8787e9717b62c0e568f6ac3565357c
Serg Mokhin <svmokhin@gmail.com>
2021-03-24 22:43:40 +0500

    3. Интерфейс Input [#483383]

---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,11 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    public void drive();
+
+    public void passengers(int passengers);
+
+    public float fuelling(float amountOfFuel);
+}
+
