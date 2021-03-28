e1a45928512eaa4550f6682ccf2af9987fb2a0a0
Serg Mokhin <svmokhin@gmail.com>
2021-03-28 17:47:53 +0500

    2.1. Тест на ValidateInput [#483407]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -21,10 +21,10 @@ public class ValidateInput implements Input {
         do {
             try {
                 value = in.askInt(question);
-                if (value < 0) {
-                    out.println("Please enter validate data again.");
-                    continue;
-                }
+//                if (value < 0) {
+//                    out.println("Please enter validate data again.");
+//                    continue;
+//                }
                 invalid = false;
             } catch (NumberFormatException nfe) {
                 out.println("Please enter validate data again.");
