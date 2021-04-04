5ba0c8d938c1ed65e84e6f819d58c35a3fc80ab0
Serg Mokhin <svmokhin@gmail.com>
2021-04-04 20:56:51 +0500

    0. Сортировка [#483358]

---
+++ src/test/java/ru/job4j/tracker/ItemSortDownByNameTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemSortDownByNameTest {
+
+    @Test
+    public void whenSortDown() {
+        Item name1 = new Item("name1");
+        Item name2 = new Item("name2");
+        Item name3 = new Item("name3");
+        Item name4 = new Item("name4");
+        Item name5 = new Item("name5");
+
+        List<Item> items = new ArrayList();
+        items.add(name1);
+        items.add(name5);
+        items.add(name2);
+        items.add(name4);
+        items.add(name3);
+        List<Item> rsl = new ArrayList();
+        rsl.add(name5);
+        rsl.add(name4);
+        rsl.add(name3);
+        rsl.add(name2);
+        rsl.add(name1);
+        Collections.sort(items, new ItemSortDownByName());
+        assertThat(items, is(rsl));
+    }
+}
