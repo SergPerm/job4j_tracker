dbb0812bb0a8dc760067c74f41571d590668e050
Serg Mokhin <svmokhin@gmail.com>
2021-04-06 21:55:26 +0500

    4. Сортировка номера [#483355]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] leftSplit = left.split("\\.");
+        String[] rightSplit = right.split("\\.");
+        int size = Math.min(rightSplit.length, leftSplit.length);
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            if (!leftSplit[i].startsWith(" ") && !rightSplit[i].startsWith(" ")) {
+                result = Integer.compare(
+                        Integer.parseInt(leftSplit[i]),
+                        Integer.parseInt(rightSplit[i]));
+                if (result != 0) {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
