d2371b1d7b444d8bf736eca94e23ffb3789b25fd
Serg Mokhin <svmokhin@gmail.com>
2021-04-12 18:19:20 +0500

    2. Отсортировать департаменты [#483350] edit

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -3,22 +3,26 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class DepDescComp implements Comparator<String> {
-    private static final int LIMIT_OF_SPLITING = 2;
 
     @Override
     public int compare(String o1, String o2) {
-        int result = o2.split("/", LIMIT_OF_SPLITING)[0]
-                .compareTo(o1.split("/", LIMIT_OF_SPLITING)[0]);
+        String[] catalog1 = o1.split("/", 2);
+        String[] catalog2 = o2.split("/", 2);
+
+//        int result = catalog2[0].compareTo(catalog1[0]);
+//        if (result != 0) {
+//            return result;
+//        } else  {
+//            return catalog1[1].compareTo(catalog2[1]);
+//        }
+
+        int result = catalog2[0].compareTo(catalog1[0]);
         if (result != 0) {
             return result;
-        } else if (o1.split("/", LIMIT_OF_SPLITING).length > 1
-                && o2.split("/", LIMIT_OF_SPLITING).length > 1) {
-            return o1.split("/", LIMIT_OF_SPLITING)[1]
-                    .compareTo(o2.split("/", LIMIT_OF_SPLITING)[1]);
+        } else if (catalog1.length > 1 && catalog2.length > 1) {
+            return catalog1[1].compareTo(catalog2[1]);
             } else {
-            return o1.split("/", LIMIT_OF_SPLITING).length
-                    > o2.split("/", LIMIT_OF_SPLITING).length
-                    ? 1 : -1;
+            return catalog1.length > catalog2.length ? 1 : -1;
         }
     }
 }
