db383b056b9fdc9c5b92722cbd3c68bd0146293d
Serg Mokhin <svmokhin@gmail.com>
2021-04-17 21:20:21 +0500

    0. Stream API [#483192]

---
+++ src/main/java/ru/job4j/stream/StreamArray.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamArray {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 5, -8, 9, -14, 0, 36, 48, -19));
+        // numbers.forEach(System.out::println);
+        List<Integer> result = numbers.stream().filter(x -> x >= 0).collect(Collectors.toList());
+        result.forEach(System.out::println);
+
+    }
+}
