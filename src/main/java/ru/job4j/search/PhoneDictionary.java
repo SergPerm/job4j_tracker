2ff5725443910694ec7bb6f296532b876a16b38b
Serg Mokhin <svmokhin@gmail.com>
2021-04-17 19:56:14 +0500

    6. Функции высшего порядка [#483179]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -15,15 +16,26 @@ public class PhoneDictionary {
      * @return Список подошедших пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> pName = person -> person.getName().contains(key);
+        Predicate<Person> pSurname = person -> person.getSurname().contains(key);
+        Predicate<Person> pPhone = person -> person.getPhone().contains(key);
+        Predicate<Person> pAddress = person -> person.getAddress().contains(key);
+        Predicate<Person> combine = pName.or(pSurname).or(pPhone).or(pAddress);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                || person.getSurname().contains(key)
-                || person.getPhone().contains(key)
-                || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
+//        ArrayList<Person> result = new ArrayList<>();
+//        for (Person person : persons) {
+//            if (person.getName().contains(key)
+//                || person.getSurname().contains(key)
+//                || person.getPhone().contains(key)
+//                || person.getAddress().contains(key)) {
+//                result.add(person);
+//            }
+//        }
         return result;
     }
 }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
@@ -14,7 +13,37 @@ public class PhoneDictionaryTest {
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("ryan");
+        ArrayList<Person> persons = phones.find("etr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("sent");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("348");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("yans");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
