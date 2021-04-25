173993f27737b47cb663f87a90de5ec118caf1b8
Serg Mokhin <svmokhin@gmail.com>
2021-04-25 15:45:47 +0500

    EasyStream [#401629] edit

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -5,56 +5,31 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
-    private static EasyStream template;
-    private static List<Integer> tmp;
+    private final List<Integer> tmp;
 
-    private EasyStream() {
-        super();
+    private EasyStream(List<Integer> source) {
+        this.tmp = source;
     }
 
     public static EasyStream of(List<Integer> source) {
-        try {
-            template = new EasyStream();
-            tmp = new ArrayList<>(source);
-            return template;
-        } catch (Exception ex) {
-            throw new UnsupportedOperationException("не верный тип данных");
-        }
+        return new EasyStream(new ArrayList<>(source));
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
         ListIterator<Integer> listIterator = tmp.listIterator();
-        try {
             while (listIterator.hasNext()) {
                 Integer element = listIterator.next();
                 listIterator.set(fun.apply(element));
             }
-            return template;
-        } catch (Exception e) {
-            throw new UnsupportedOperationException(e.getMessage());
-        }
+            return this;
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-        ListIterator<Integer> listIterator = tmp.listIterator();
-        try {
-            while (listIterator.hasNext()) {
-                Integer element = listIterator.next();
-                if (!fun.test(element)) {
-                    listIterator.remove();
-                }
-            }
-            return template;
-        } catch (Exception e) {
-            throw new UnsupportedOperationException(e.getMessage());
-        }
+        tmp.removeIf(element -> !fun.test(element));
+        return this;
     }
 
     public List<Integer> collect() {
-        try {
             return new ArrayList<>(tmp);
-        } catch (Exception e) {
-            throw new UnsupportedOperationException(e.getMessage());
-        }
     }
 }
