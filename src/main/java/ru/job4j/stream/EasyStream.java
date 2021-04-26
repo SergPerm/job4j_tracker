61ce884d82f50b7b1f7617b3473e1bd9f9ea254b
Serg Mokhin <svmokhin@gmail.com>
2021-04-26 19:15:41 +0500

    EasyStream [#401629] edit3

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -37,3 +37,61 @@ public class EasyStream {
             return List.copyOf(tmp);
     }
 }
+//import java.util.ArrayList;
+//        import java.util.List;
+//        import java.util.function.Function;
+//        import java.util.function.Predicate;
+//
+//public class EasyStream {
+//    private List<Integer> source;
+//
+//    private EasyStream() {
+//    }
+//
+//    public static EasyStream of(List<Integer> source) {
+//        return new Builder()
+//                .buildSource(source)
+//                .build();
+//    }
+//
+//    public EasyStream map(Function<Integer, Integer> fun) {
+//        List<Integer> rsl = new ArrayList<>();
+//        for (Integer i : source) {
+//            rsl.add(fun.apply(i));
+//        }
+//        return new Builder()
+//                .buildSource(rsl)
+//                .build();
+//    }
+//
+//    public EasyStream filter(Predicate<Integer> fun) {
+//        List<Integer> rsl = new ArrayList<>();
+//        for (Integer i : source) {
+//            if (fun.test(i)) {
+//                rsl.add(i);
+//            }
+//        }
+//        return new Builder()
+//                .buildSource(rsl)
+//                .build();
+//    }
+//
+//    public List<Integer> collect() {
+//        return List.copyOf(source);
+//    }
+//
+//    static class Builder {
+//        private List<Integer> source;
+//
+//        Builder buildSource(List<Integer> source) {
+//            this.source = source;
+//            return this;
+//        }
+//
+//        EasyStream build() {
+//            EasyStream stream = new EasyStream();
+//            stream.source = source;
+//            return stream;
+//        }
+//    }
+//}
